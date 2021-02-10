d239795c003af37db4712ab8d63ce965414df070
Richard <richard.d.merced@gmail.com>
2021-02-09 17:58:52 -0800

    Start of new React Project

---
+++ components/Title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Title = () => {
+    return <div id="title">
+    <h1>
+     Stranger Thing's
+    </h1>
+    <h5>
+Just a development set up for Stranger things
+    </h5>
+  </div>
+}
+export default Title;
---
+++ index.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+
+import {
+ 
+    Title
+  } from './components';
+
+
+const App = () => {
+  return <div className="app">
+   
+ <Title />
+
+  </div>
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
